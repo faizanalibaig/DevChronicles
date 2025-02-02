342644d265d45b65e19447a527f0f9deecf2caa7
faizanalibaig <riazfiaz6181@gmail.com>
2025-02-02 22:12:28 +0500

    backend work in-progress

--- server/controllers/blog.controller.js
+++ server/controllers/blog.controller.js
@@ -11,3 +11,16 @@ exports.create = async function (req, res) {
     res.status(400).json({ message: error.message });
   }
 };
+
+exports.find = async function (req, res) {
+  try {
+    const id = req.params.id;
+    const blog = await Blog.findById(id);
+    res.status(200).send({
+      message: 'Blog post found successfully',
+      data: blog,
+    });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
